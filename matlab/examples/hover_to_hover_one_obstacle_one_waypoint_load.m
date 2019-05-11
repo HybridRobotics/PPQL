ffde279b9a321d0cc913a156cdc48be63af07a7c
Jun Zeng <zengjunsjtu@berkeley.edu>
2019-05-10 19:01:51 -0700

    Add example files

---
+++ matlab/examples/hover_to_hover_one_obstacle_one_waypoint_load.m
@@ -0,0 +1,22 @@
+clear
+close all
+
+params = SystemParameters();
+
+local_setting1 = LocalSetting();
+local_setting1.defineTraveltimeBounded(1,4);
+local_setting1.addWaypoint(Waypoint(25,'load',[0;0;2.5],0.01));
+
+global_setting = GlobalSetting();
+obstacle1 = Polyhedron('lb',[-0.5;0.5;1],'ub',[0.5;0.5;2]);
+global_setting.addObstacle(obstacle1);
+
+path_planning_setting = PathPlanningSetting();
+path_planning_setting.addLocalSetting(local_setting1);
+path_planning_setting.addGlobalSetting(global_setting);
+
+initial = QuadLoadState([0;-2.3;1.5],[0;0;0],[0;0;-1],[0;0;0],eye(3),[0;0;0]);
+final = QuadLoadState([0;1.7;1.5],[0;0;0],[0;0;-1],[0;0;0],eye(3),[0;0;0]);
+problem = PathPlanningFormulation(params,path_planning_setting,initial,final);
+problem.solve();
+problem.saveData('results.mat');
---
+++ matlab/examples/hover_to_hover_three_waypoints_load.m
@@ -0,0 +1,23 @@
+clear
+close all
+
+params = SystemParameters();
+
+local_setting1 = LocalSetting();
+local_setting1.defineTraveltimeBounded(1,4);
+local_setting1.num_nodes = 100;
+local_setting1.addWaypoint(Waypoint(26,'load',[-1;2;1.5],0.01));
+local_setting1.addWaypoint(Waypoint(51,'load',[1;2;1.5],0.01));
+local_setting1.addWaypoint(Waypoint(76,'load',[1;0;1.5],0.01));
+
+global_setting = GlobalSetting();
+
+path_planning_setting = PathPlanningSetting();
+path_planning_setting.addLocalSetting(local_setting1);
+path_planning_setting.addGlobalSetting(global_setting);
+
+initial = QuadLoadState([-1;0;1.5],[0;0;0],[0;0;-1],[0;0;0],eye(3),[0;0;0]);
+final = QuadLoadState([-1;0;1.5],[0;0;0],[0;0;-1],[0;0;0],eye(3),[0;0;0]);
+problem = PathPlanningFormulation(params,path_planning_setting,initial,final);
+problem.solve();
+problem.saveData('results.mat');
