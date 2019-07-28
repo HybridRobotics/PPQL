493e87d0d9df01fd910ce16ffebf7c9069c45a9c
Jun Zeng <zengjunsjtu@berkeley.edu>
2019-07-27 23:00:28 -0700

    Add python interface and unit tests

--- python/data_collector.py
+++ python/data_collector.py
@@ -4,39 +4,84 @@ from scipy import interpolate
 class TrajectoryCollector:
 	def __init__(self,filename):
 		data = sio.loadmat('../data/'+filename)
-		self.time = data['time']
-		self.xl = data['xl']
-		self.vl = data['vl']
-		self.al = data['al']
-		self.dal = data['dal']
-		self.d2al = data['d2al']
-		self.d3al = data['d3al']
-		self.d4al = data['d4al']
-		self.num_nodes = self.time.shape[1]-1
-		self.dt = (self.time[0,self.num_nodes]-self.time[0,0])/self.num_nodes
+		self.time_list = data['time_list']
+		self.status_list = data['status_list']
+		self.xQ_list = data['xQ_list']
+		self.xL_list = data['xL_list']
+		self.vL_list = data['vL_list']
+		self.aL_list = data['aL_list']
+		self.daL_list = data['daL_list']
+		self.d2aL_list = data['d2aL_list']
+		self.d3aL_list = data['d3aL_list']
+		self.d4aL_list = data['d4aL_list']
+
+	# get the size of trajectory
+	def get_trajectory_size(self):
+		return len(self.time_list[0,:])
+
+	# get the index most closed to the given time
+	def get_index(self, time):
+		if time <= self.time_list[0, 0]:
+			# trajectory not started
+			return -1
+		elif time >= self.time_list[0, -1]:
+			# trajectory finished
+			return self.get_trajectory_size()-1
+		else:
+			# BFS to get the index
+			left = 0
+			right = self.get_trajectory_size()-1
+			while (right > left + 1):
+				mid = (int) (left+right)/2
+				if time > self.time_list[0, mid]:
+					left = mid
+				elif time < self.time_list[0, mid]:
+					right = mid
+				else:
+					return self.time_list[0, mid]
+			return left
 
 	def interpolation_linear(self, time):
-		ind = int(time/self.dt)
-		gamma = (time-self.dt*ind)/self.dt
-		print()
-		if ind < self.num_nodes:
-			xl = self.xl[:,ind]*gamma + self.xl[:,ind+1]*(1-gamma)
-			vl = self.vl[:,ind]*gamma + self.vl[:,ind+1]*(1-gamma)
-			al = self.al[:,ind]*gamma + self.al[:,ind+1]*(1-gamma)
-			dal = self.dal[:,ind]*gamma + self.dal[:,ind+1]*(1-gamma)
-			d2al = self.d2al[:,ind]*gamma + self.d2al[:,ind+1]*(1-gamma)
-			d3al = self.d3al[:,ind]*gamma + self.d3al[:,ind+1]*(1-gamma)
-			d4al = self.d4al[:,ind]*gamma + self.d4al[:,ind+1]*(1-gamma)
+		ind = self.get_index(time)
+		if ind == -1:
+			time = self.time_list[0, 0]
+			status = self.status_list[0, 0]
+			xQ = self.xQ_list[:, 0]
+			xL = self.xL_list[:, 0]
+			vL = self.vL_list[:, 0]
+			aL = self.aL_list[:, 0]
+			daL = self.daL_list[:, 0]
+			d2aL = self.d2aL_list[:, 0]
+			d3aL = self.d3aL_list[:, 0]
+			d4aL = self.d4aL_list[:, 0]
+		elif ind == self.get_trajectory_size() - 1:
+			# use last pose of the trajectory
+			time = self.time_list[0, ind]
+			status = self.status_list[0, ind]
+			xQ = self.xQ_list[:, ind]
+			xL = self.xL_list[:, ind]
+			vL = self.vL_list[:, ind]
+			aL = self.aL_list[:, ind]
+			daL = self.daL_list[:, ind]
+			d2aL = self.d2aL_list[:, ind]
+			d3aL = self.d3aL_list[:, ind]
+			d4aL = self.d4aL_list[:, ind]
+			return {'time': time, 'status': status, 'xQ': xQ, 'xL': xL, 'vL': vL, 'aL': aL, 'daL': daL, 'd2aL': d2aL, 'd3aL': d3aL, 'd4aL': d4aL}
 		else:
-			xl = self.xl[:,ind]
-			vl = self.vl[:,ind]
-			al = self.al[:,ind]
-			dal = self.dal[:,ind]
-			d2al = self.d2al[:,ind]
-			d3al = self.d3al[:,ind]
-			d4al = self.d4al[:,ind]
+			gamma = (time-self.time_list[0, ind])/(self.time_list[0, ind+1] - self.time_list[0, ind])
+			# linear interpolation between poses
+			time = self.time_list[0, ind]*gamma+self.time_list[0, ind+1]*(1-gamma) 
+			status = self.status_list[0, ind]*gamma+self.status_list[0, ind+1]*(1-gamma)
+			xQ = self.xQ_list[:, ind]*gamma+self.xQ_list[:, ind+1]*(1-gamma)
+			xL = self.xL_list[:, ind]*gamma+self.xL_list[:, ind+1]*(1-gamma)
+			vL = self.vL_list[:, ind]*gamma+self.vL_list[:, ind+1]*(1-gamma)
+			aL = self.aL_list[:, ind]*gamma+self.aL_list[:, ind+1]*(1-gamma)
+			daL = self.daL_list[:, ind]*gamma+self.daL_list[:, ind+1]*(1-gamma)
+			d2aL = self.d2aL_list[:, ind]*gamma+self.d2aL_list[:, ind+1]*(1-gamma)
+			d3aL = self.d3aL_list[:, ind]*gamma+self.d3aL_list[:, ind+1]*(1-gamma)
+			d4aL = self.d4aL_list[:, ind]*gamma+self.d4aL_list[:, ind+1]*(1-gamma)
 		
-		return {'xl': xl, 'vl': vl, 'al': al, 'dal': dal, 'd2al': d2al, 'd3al': d3al, 'd4al': d4al}
+		return {'time': time, 'status': status, 'xQ': xQ, 'xL': xL, 'vL': vL, 'aL': aL, 'daL': daL, 'd2aL': d2aL, 'd3aL': d3aL, 'd4aL': d4aL}
 
 	def interpolation_quadratic(self, time):
 		pass
---
+++ python/data_collector_test.py
@@ -0,0 +1,65 @@
+from data_collector import TrajectoryCollector
+import numpy as np
+import time
+
+# intialize the trajectory collector, containing all the flat outputs
+data = TrajectoryCollector('../data/hover_to_hover.mat')
+np.set_printoptions(precision=4)
+# call the flat outputs at a specific timestamp 
+
+# TEST 1: not start yet, use the initial state
+time_start = time.clock()
+timestamp = -1
+flat_outputs = data.interpolation_linear(timestamp)
+print('Read data at timestamp t = ', timestamp)
+# display the flat outputs
+print('timestamp',flat_outputs['time'])
+print('status',flat_outputs['status'])
+print('quadrotor position',flat_outputs['xQ'])
+print('load position',flat_outputs['xL'])
+print('load velocity',flat_outputs['vL'])
+print('load acceleration',flat_outputs['aL'])
+print('load 1st derivative of acceleration',flat_outputs['daL'])
+print('load 2nd derivative of acceleration',flat_outputs['d2aL'])
+print('load 3rd derivative of acceleration',flat_outputs['d3aL'])
+print('load 4th derivative of acceleration',flat_outputs['d4aL'])
+time_elapsed = (time.clock() - time_start)
+print('computational time for getting values', time_elapsed)
+
+# TEST 2: during the path planning, get values from interpolation
+time_start = time.clock()
+timestamp = 3
+flat_outputs = data.interpolation_linear(timestamp)
+print('Read data at timestamp t = ', timestamp)
+# display the flat outputs
+print('timestamp',flat_outputs['time'])
+print('status',flat_outputs['status'])
+print('quadrotor position',flat_outputs['xQ'])
+print('load position',flat_outputs['xL'])
+print('load velocity',flat_outputs['vL'])
+print('load acceleration',flat_outputs['aL'])
+print('load 1st derivative of acceleration',flat_outputs['daL'])
+print('load 2nd derivative of acceleration',flat_outputs['d2aL'])
+print('load 3rd derivative of acceleration',flat_outputs['d3aL'])
+print('load 4th derivative of acceleration',flat_outputs['d4aL'])
+time_elapsed = (time.clock() - time_start)
+print('computational time for getting values', time_elapsed)
+
+# TEST 3: not start yet, use the final state
+time_start = time.clock()
+timestamp = 10
+flat_outputs = data.interpolation_linear(timestamp)
+print('Read data at timestamp t = ', timestamp)
+# display the flat outputs
+print('timestamp',flat_outputs['time'])
+print('status',flat_outputs['status'])
+print('quadrotor position',flat_outputs['xQ'])
+print('load position',flat_outputs['xL'])
+print('load velocity',flat_outputs['vL'])
+print('load acceleration',flat_outputs['aL'])
+print('load 1st derivative of acceleration',flat_outputs['daL'])
+print('load 2nd derivative of acceleration',flat_outputs['d2aL'])
+print('load 3rd derivative of acceleration',flat_outputs['d3aL'])
+print('load 4th derivative of acceleration',flat_outputs['d4aL'])
+time_elapsed = (time.clock() - time_start)
+print('computational time for getting values', time_elapsed)
